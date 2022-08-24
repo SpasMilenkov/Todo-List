cd85973a980ea88d288c4a10982074f0b2d07acf
Clash2453 <spasmilenkov123@gmail.com>
2022-08-24 13:41:26 +0300

    Fix onclick class toggle

--- src/ui/createNavigation.js
+++ src/ui/createNavigation.js
@@ -1,27 +1,26 @@
 import '../style.css';
 import menuImage from '../icons/menu.png';
+const navBar = document.createElement('nav');
 
+function navigation(buttonTheme, theme) {
 
-const navigation =  (buttonTheme, theme) =>{
-    const navBar = document.createElement('nav');
     const ul = document.createElement('ul');
     const menu = document.createElement('li');
     const menuButton = new Image(48, 48);
 
     menuButton.src = menuImage;
-    menuButton.addEventListener('click', toggleExpand(navBar))
+    menuButton.addEventListener('click', toggleExpand);
     menu.appendChild(menuButton);
-    navBar.appendChild(ul)
+    navBar.appendChild(ul);
     ul.appendChild(menu);
-    navBar.classList.add(theme)
+    navBar.classList.add(theme);
 
-    ul.classList.add('flex')
+    ul.classList.add('flex');
     document.body.appendChild(navBar);
-};
+}
 
-function toggleExpand(navBar){
+function toggleExpand(){
     navBar.classList.toggle('expanded');       
-
 }
 
 export default navigation
