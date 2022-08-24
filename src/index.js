01a427cbdb5dee62d4c69e973ca9eeb9d59d029b
Clash2453 <spasmilenkov123@gmail.com>
2022-08-24 17:11:13 +0300

    Call the createTodo function on load

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-import './style.css'
+import {setTheme }from "./ui/applySystemTheme";
+import { createButton } from "./ui/createVisualToDo";
+const contents = document.createElement('div');
+contents.id = 'content'
+setTheme()
+
+document.body.appendChild(contents);
+contents.appendChild(createButton());
+export default contents;
