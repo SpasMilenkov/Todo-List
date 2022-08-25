a2d1282a80a8dbb75331a3e37dbae310d703672d
Clash2453 <spasmilenkov123@gmail.com>
2022-08-25 15:01:20 +0300

    Create the ToDo class

---
+++ src/background/createToDo.js
@@ -0,0 +1,14 @@
+class toDo{
+    constructor(name, description, category){
+        this.name = name;
+        this.description = description;
+        this.status = false;
+        this.category = category;
+    }
+
+    complete() {
+        this.status = complete
+    }
+}
+
+export default toDo;
