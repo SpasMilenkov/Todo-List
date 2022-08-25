f32e558f778156e1089357667435f2fa1ecd4b85
Clash2453 <spasmilenkov123@gmail.com>
2022-08-25 21:44:01 +0300

    Update the todo class

--- src/background/createToDo.js
+++ src/background/createToDo.js
@@ -1,10 +1,22 @@
+import { getAll } from "./mediator";
+
 class toDo{
+
     constructor(name, description, category){
         this.name = name;
         this.description = description;
         this.status = false;
         this.category = category;
+        this.id = this.calcId();
     }
+
+    calcId(){
+        const list = getAll().count
+        if(list === undefined){
+            return 0
+        }
+        return list - 1;
+    };
 
     complete() {
         this.status = complete
