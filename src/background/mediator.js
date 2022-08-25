3cfe98355ce83938aab89ca27a15008bb9dc510d
Clash2453 <spasmilenkov123@gmail.com>
2022-08-25 21:45:05 +0300

    Export functions rework add toDo function

--- src/background/mediator.js
+++ src/background/mediator.js
@@ -1,13 +1,14 @@
 import toDo from "./createToDo";
 let toDos = [];
 
-function addToDo(name, description, category ){
-    const task = new toDo(name, description, category);
-    toDos.unshift(task);
-}
+function addToDo(task){toDos.unshift(task);}
 
 function deleteToDo(id){return toDos.filter(e=> e.id == id)[0];};
 
 function completed(){return addToDo.filter(e => e.status == true);};
 
 function pending(){return addToDo.filter(e=> e.status == false)};
+
+function getAll(){return toDos;}
+
+export {addToDo, deleteToDo, completed, pending, getAll};
