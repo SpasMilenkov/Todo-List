9e5bc6a36fd1c2f384e9256d70efd39abc6df786
Clash2453 <spasmilenkov123@gmail.com>
2022-08-26 20:53:54 +0300

    Add function to render categories page

--- src/ui/toDoForm.js
+++ src/ui/toDoForm.js
@@ -15,3 +15,20 @@ function renderForm(){
     container.classList.add('form-light-theme');
     return container;
 }
+//Generates the category selection page
+function categoryChoice(){
+
+    const mainTitle = document.createElement('h1')
+    mainTitle.textContent = 'Categories '
+
+    container.appendChild(mainTitle);
+    container.appendChild(radioButtonFactory('work', 'Work'));
+    container.appendChild(radioButtonFactory('sport', 'Sport'));
+    container.appendChild(radioButtonFactory('relax', 'Relax'));
+
+    const nextButton = document.createElement('button');
+    nextButton.textContent = 'Next'
+    nextButton.addEventListener('click', addInfo);
+
+}
+
