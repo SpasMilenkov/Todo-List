b6802486ad94870e24d2379c955dd3977ab1c2a4
Spas Milenkov <87969078+Clash2453@users.noreply.github.com>
2023-02-04 14:10:03 +0200

    Add the base clock component

---
+++ TODO Application/src/components/ClockComponent.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <h1 class="time">{{ displayedTime }}</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data: function () {
+    return {
+      displayedTime: "",
+    };
+  },
+  methods: {
+    updateTime() {
+      setTimeout(this.updateTime, 1000);
+      let time = new Date();
+      this.displayedTime = time.toLocaleTimeString();
+    },
+  },
+  mounted() {
+    this.updateTime();
+  },
+}
+</script>
+
+<style scoped>
+.container {
+  width: 30rem;
+  height: 30rem;
+  border-radius: 50%;
+  background-color: var(--gray);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.time {
+  color: whitesmoke;
+  font-size: 4rem;
+  font-family: "Robot", sans-serif;
+}
+</style>
