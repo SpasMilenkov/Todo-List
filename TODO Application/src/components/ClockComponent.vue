f4d9659ec6308a17de73c5f3ad6c3e9f9ce2ce25
Spas Milenkov <87969078+Clash2453@users.noreply.github.com>
2023-02-04 15:43:47 +0200

    Dynamically change clock background

--- TODO Application/src/components/ClockComponent.vue
+++ TODO Application/src/components/ClockComponent.vue
@@ -10,6 +10,7 @@ export default {
   data: function () {
     return {
       displayedTime: "",
+      clock: "",
     };
   },
   methods: {
@@ -17,6 +18,17 @@ export default {
       setTimeout(this.updateTime, 1000);
       let time = new Date();
       this.displayedTime = time.toLocaleTimeString();
+      let hours = time.getHours();
+
+      if (hours < 12) {
+        this.clock = 'url("/public/images/morning.png")';
+        return;
+      }
+      if (hours < 16 && hours > 12) {
+        this.clock = 'url("/public/images/noon.png")';
+        return;
+      }
+      this.clock = 'url("/public/images/evening.png")';
     },
   },
   mounted() {
@@ -24,13 +36,42 @@ export default {
   },
 }
 </script>
-
+<template>
+  <div class="container">
+    <div class="mask">
+      <div class="fill">
+        <h1 class="time">{{ displayedTime }}</h1>
+      </div>
+    </div>
+  </div>
+</template>
 <style scoped>
 .container {
   width: 30rem;
   height: 30rem;
   border-radius: 50%;
-  background-color: var(--gray);
+  /* background-color: var(--gray); */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.container .fill {
+  width: 29rem;
+  height: 29rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 15rem;
+  background-image: v-bind(clock);
+  background-position: left;
+  background-size: cover;
+  background-color: rgba(20, 20, 20, 0.15);
+  background-blend-mode: darken;
+  background-repeat: no-repeat;
+}
+.container .mask {
+  width: 30rem;
+  height: 30rem;
   display: flex;
   align-items: center;
   justify-content: center;
