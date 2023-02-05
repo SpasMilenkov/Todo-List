4c5655d0741d31240bf19fe439023df1887b69fb
Spas Milenkov <87969078+Clash2453@users.noreply.github.com>
2023-02-05 13:50:38 +0200

    Add component for displaying tasks

---
+++ TODO Application/src/components/TodoDisplayComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    
+  </div>
+</template>
+<script></script>
+
+<style scoped>
+.container {
+  width: 30rem;
+  height: 30rem;
+  box-shadow: 0 0 1rem 0 rgba(0, 0, 0, 0.2);
+  border-radius: 10px;
+  position: relative;
+  z-index: 1;
+  background: inherit;
+  overflow: hidden;
+}
+.container:before {
+  content: "";
+  position: absolute;
+  background: inherit;
+  z-index: -1;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  box-shadow: inset 0 0 2000px rgba(255, 255, 255, 0.5);
+  filter: blur(10px);
+  margin: -20px;
+}
+</style>
