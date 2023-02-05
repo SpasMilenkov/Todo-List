8c6dcca43b1c6d01fab5707239106949623707aa
Spas Milenkov <87969078+Clash2453@users.noreply.github.com>
2023-02-05 13:51:05 +0200

    Add styles to the main content

--- TODO Application/src/views/HomeView.vue
+++ TODO Application/src/views/HomeView.vue
@@ -1,9 +1,21 @@
 <script setup>
 import ClockComponent from '../components/ClockComponent.vue';
+import TodoDisplayComponent from '../components/TodoDisplayComponent.vue';
 </script>
 
 <template>
-  <main>
+  <main id="home-main">
     <ClockComponent></ClockComponent>
+    <TodoDisplayComponent></TodoDisplayComponent>
   </main>
 </template>
+<style scoped>
+#home-main{
+  display: flex;
+  gap: 2rem;
+  padding: 1rem;
+  height: calc(100vh - 2rem);
+  width: calc(100vw - 2rem);
+  background-attachment: fixed;
+}
+</style>
