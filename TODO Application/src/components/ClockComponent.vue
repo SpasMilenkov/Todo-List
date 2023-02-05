7b47f964a519c54cc1e47b547e9bff1a52203de3
Spas Milenkov <87969078+Clash2453@users.noreply.github.com>
2023-02-05 13:49:46 +0200

    Add frosted effect to the component

--- TODO Application/src/components/ClockComponent.vue
+++ TODO Application/src/components/ClockComponent.vue
@@ -26,11 +26,11 @@ export default {
       }
 
       if (hours < 16 && hours > 12) {
-        this.clock = 'url("/public/images/noon.png")';
+        this.clock = 'url("/public/images/morning.png")';
         return;
       }
       
-      this.clock = 'url("/public/images/evening.png")';
+      this.clock = 'url("/public/images/night.png")';
     },
   },
   mounted() {
@@ -38,25 +38,46 @@ export default {
   },
 }
 </script>
+<!-- eslint-disable prettier/prettier -->
 <template>
   <div class="container">
     <div class="mask">
       <div class="fill">
-        <h1 class="time">{{ displayedTime }}</h1>
       </div>
     </div>
+    <h1 class="time">{{ displayedTime }}</h1>
+
   </div>
 </template>
-
+<!-- eslint-disable prettier/prettier -->
 <style scoped>
 .container {
   width: 30rem;
-  height: 30rem;
-  border-radius: 50%;
-  /* background-color: var(--gray); */
+  height: 50rem;
+  box-shadow: 0 0 1rem 0 rgba(0, 0, 0, 0.2);
+  position: relative;
   display: flex;
+  flex-direction: column;
+  padding: 1rem;
   align-items: center;
-  justify-content: center;
+  gap: 1rem;
+  z-index: 1;
+  background: inherit;
+  overflow: hidden;
+  border-radius: 10px;
+}
+.container:before {
+  content: "";
+  position: absolute;
+  background: inherit;
+  z-index: -1;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  box-shadow: inset 0 0 2000px rgba(255, 255, 255, 0.5);
+  filter: blur(25px);
+  margin: -20px;
 }
 .container .fill {
   width: 29rem;
@@ -66,9 +87,9 @@ export default {
   justify-content: center;
   border-radius: 15rem;
   background-image: v-bind(clock);
-  background-position: left;
+  background-position: center;
   background-size: cover;
-  background-color: rgba(20, 20, 20, 0.15);
+  /* background-color: rgba(20, 20, 20, 0.15); */
   background-blend-mode: darken;
   background-repeat: no-repeat;
 }
