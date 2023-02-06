4df3cf98d8b6763957b9fa7f68add62f6d4c9005
Spas Milenkov <87969078+Clash2453@users.noreply.github.com>
2023-02-06 22:12:25 +0200

    Call api on page load to get accurate geolocation

--- TODO Application/src/views/HomeView.vue
+++ TODO Application/src/views/HomeView.vue
@@ -1,11 +1,35 @@
 <script setup>
 import ClockComponent from '../components/ClockComponent.vue'
 import TodoDisplayComponent from '../components/TodoDisplayComponent.vue'
+import axios from 'axios'
+import { onMounted } from 'vue'
+
+let sunset = 0
+let sunrise = 0
+async function getDayTime() {
+  let lat = 0.0
+  let long = 0.0
+  navigator.geolocation.getCurrentPosition((position) => {
+    lat = position.coords.latitude.toFixed(4)
+    long = position.coords.longitude.toFixed(4)
+  })
+  await axios
+    .get(`https://api.sunrise-sunset.org/json?lat=${lat}lng=${long}`)
+    .then((result) => result.data.results)
+    .then((data) => {
+      sunset = data.sunset.split(':')[0]
+      sunrise = data.sunrise.split(':')[0]
+    })
+    .catch(() => console.log('bad'))
+}
+onMounted(() => {
+  getDayTime()
+})
 </script>
 
 <template>
   <main id="home-main">
-    <ClockComponent></ClockComponent>
+    <ClockComponent :sunrise="sunrise" :sunset="sunset"></ClockComponent>
     <TodoDisplayComponent></TodoDisplayComponent>
   </main>
 </template>
