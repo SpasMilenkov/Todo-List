2fdb203b3b34c38dda2d5a36ff7b621e9390a2c3
Spas Milenkov <87969078+Clash2453@users.noreply.github.com>
2023-02-07 22:56:13 +0200

    Add navigation bar and move the clock there

--- TODO Application/src/App.vue
+++ TODO Application/src/App.vue
@@ -1,11 +1,36 @@
 <script setup>
-import HomeView from './views/HomeView.vue';
+import HomeView from './views/HomeView.vue'
+import ClockComponent from './components/ClockComponent.vue'
 </script>
 
 <template>
+  <nav id="navigation" class="container">
+    <div class="wrapper">
+      <ClockComponent></ClockComponent>
+    </div>
+    <ul id="navigation-container">
+      <li><a href="">Dummy 1</a></li>
+      <li><a href="">Dummy 2</a></li>
+    </ul>
+  </nav>
   <HomeView></HomeView>
 </template>
 
 <style scoped>
-
+#navigation {
+  width: calc(100vw - 2rem);
+  height: 3rem;
+  border-radius: 0px;
+}
+#navigation-container {
+  width: 90%;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 1rem;
+}
+.wrapper{
+  widows: 20vw;
+  min-width: 15rem;
+}
 </style>
