aa54471f86cfb74c11abbc36956992df1ef53c3d
Spas Milenkov <87969078+Clash2453@users.noreply.github.com>
2023-02-07 22:54:03 +0200

    Simplify the clock, separate theme image

--- TODO Application/src/components/ClockComponent.vue
+++ TODO Application/src/components/ClockComponent.vue
@@ -1,13 +1,9 @@
 <script>
 export default {
-  props: ['sunrise', 'sunset'],
   data: function () {
     return {
       displayedTime: '',
       clock: '',
-      offset: 'linear-gradient(0deg, rgb(229, 172, 59) 50%, #3d4f5d 50%)',
-      quote: '',
-      author: '',
     }
   },
   methods: {
@@ -15,104 +11,20 @@ export default {
       setTimeout(this.updateTime, 1000)
       let time = new Date()
       this.displayedTime = time.toLocaleTimeString()
-      let hours = time.getHours()
-
-      if (hours <= 12) {
-        this.clock = 'url("/public/images/morning.png")'
-        return
-      }
-      if (hours < 16 && hours > 12) {
-        this.clock = 'url("/public/images/noon.png")'
-        return
-      }
-      if (hours >= 16 && hours < 20) {
-        this.clock = 'url("/public/images/evening.png")'
-        return
-      }
-      this.clock = 'url("/public/images/night.png")'
-    },
-    async generateQuote() {
-      const rnd = Math.floor(Math.random() * 1642)
-      await fetch('https://type.fit/api/quotes')
-        .then((response) => response.json())
-        .then((data) => {
-          this.quote = `"${data[rnd].text}"`
-          this.author = data[rnd].author == null ? 'Unknown' : data[rnd].author
-        })
-      console.log(this.quote)
     },
   },
   mounted() {
     this.updateTime()
-    this.generateQuote()
   },
 }
 </script>
 <template>
-  <div class="container">
-    <div class="fill">
-      <h1 class="time">{{ displayedTime }}</h1>
-    </div>
-    <h1 id="quote">
-      {{ quote }} <br />
-      <br />
-      {{ author }}
-    </h1>
-  </div>
+  <h1 class="time">{{ displayedTime }}</h1>
 </template>
 <style scoped>
-.container {
-  width: 30rem;
-  min-height: 50rem;
-  height: calc(100vh - 4rem);
-  box-shadow: 0 0 1rem 0 rgba(0, 0, 0, 0.2);
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  padding: 1rem;
-  align-items: center;
-  gap: 5rem;
-  z-index: 1;
-  background: inherit;
-  overflow: hidden;
-  border-radius: 10px;
-}
-.container:before {
-  content: '';
-  position: absolute;
-  background: inherit;
-  z-index: -1;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  box-shadow: inset 0 0 2000px rgba(255, 255, 255, 0.5);
-  filter: blur(25px);
-  margin: -20px;
-}
-.container .fill {
-  width: 30rem;
-  height: 30rem;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 15rem;
-  background-image: v-bind(clock);
-  background-position: center;
-  background-size: cover;
-  /* background-color: rgba(20, 20, 20, 0.15); */
-  background-blend-mode: darken;
-  background-repeat: no-repeat;
-}
 .time {
   color: white;
-  font-size: 4rem;
+  font-size: 2rem;
   font-family: 'Robot', sans-serif;
-}
-#quote {
-  font-family: var(--main-title);
-  font-size: 1.4rem;
-  color: white;
-  text-align: center;
 }
 </style>
