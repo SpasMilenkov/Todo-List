a31d966ce5aa5e5a0e369e1a5554d7de3ed29c73
Spas Milenkov <87969078+Clash2453@users.noreply.github.com>
2023-02-09 21:12:25 +0200

    Add line chart component

---
+++ TODO Application/src/components/LineChartComponent.vue
@@ -0,0 +1,48 @@
+<template>
+  <Line :data="data" :options="options" />
+</template>
+
+<script>
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js'
+import { Line } from 'vue-chartjs'
+import * as chartConfig from './chartConfig.js'
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+)
+
+export default {
+  name: 'App',
+  props: ['title', 'labels', 'dataset'],
+  components: {
+    Line,
+  },
+  data() {
+    return chartConfig
+  },
+  mounted() {
+    chartConfig.data.datasets = {
+      label: this.title,
+      backgroundColor: '#f87979',
+      data: this.dataset,
+    }
+    chartConfig.data.labels = this.labels
+    chartConfig.data.labels = this.data
+  },
+}
+</script>
