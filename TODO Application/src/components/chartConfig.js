581d198450692a0c2954b3469a3aadad5f16e325
Spas Milenkov <87969078+Clash2453@users.noreply.github.com>
2023-02-09 21:15:01 +0200

    Add chart config file

---
+++ TODO Application/src/components/chartConfig.js
@@ -0,0 +1,17 @@
+export const data = {
+  labels: [],
+  datasets: [
+    {
+      label: '',
+      backgroundColor: '',
+      data: [],
+    },
+  ],
+}
+
+export const options = {
+  responsive: true,
+  maintainAspectRatio: false,
+  borderColor: 'rgb(75, 192, 192)',
+  tension: 1,
+}
