0b13d5832cbf071983997c840b3a555d4acd4aa5
Spas Milenkov <spasmilenkov123@gmail.com>
2023-02-11 20:58:36 +0200

    Add mitt

--- TODO Application/src/main.js
+++ TODO Application/src/main.js
@@ -1,14 +1,14 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import mitt from 'mitt'
 import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
 
 const app = createApp(App)
-
+const emitter = mitt()
+app.config.globalProperties.emitter = emitter
 app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
