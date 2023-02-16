ac85f300b5b4d0e97e5e9e28a811ce8787fa1de4
Spas Milenkov <87969078+Clash2453@users.noreply.github.com>
2023-02-16 23:16:12 +0200

    Fix issue with the main container

--- TODO Application/src/views/HomeView.vue
+++ TODO Application/src/views/HomeView.vue
@@ -3,7 +3,6 @@ import QuoteComponent from '../components/QuoteComponent.vue'
 import TodoCreateComponent from '../components/TodoCreateComponent.vue'
 import ForecastComponent from '../components/ForecastComponent.vue'
 import TodoDisplayComponent from '../components/TodoDisplayComponent.vue'
-import DoneDisplayComponent from '../components/DoneDisplayComponent.vue'
 export default {
   data: function () {
     return {
@@ -12,32 +11,44 @@ export default {
       clock: '',
       lat: 0.0,
       long: 0.0,
+      brightness: 'inset 0 0 2000px rgba(255, 255, 255, 0.5)',
+      background:
+        'linear-gradient(45deg, #444558, #b35c9e, rgba(230,196,156,1))',
     }
   },
   methods: {
     syncTheme() {
       const time = new Date()
       const hours = time.getHours()
-
+      
       if (hours <= 12) {
         this.clock = 'url("/images/morning.png")'
+        this.background =
+          'linear-gradient(45deg, #444558, #b35c9e, rgba(230,196,156,1))'
         setTimeout(this.syncTheme, 60000)
 
         return
       }
       if (hours < 16 && hours > 12) {
         this.clock = 'url("/images/noon.png")'
+        this.background = 'linear-gradient(330deg, #e5ac3b, #c87225, #964424)'
         setTimeout(this.syncTheme, 60000)
 
         return
       }
       if (hours >= 16 && hours < 20) {
         this.clock = 'url("/images/evening.png")'
+        this.brightness = `inset 0 0 2000px rgba(255, 255, 255, 0.3)`
+        this.background =
+          'linear-gradient(330deg, #aa5c46, #6d2e61, rgba(26,21,49,1))'
         setTimeout(this.syncTheme, 60000)
 
         return
       }
       this.clock = 'url("/images/night.png")'
+      this.brightness = `inset 0 0 2000px rgba(255, 255, 255, 0.3)`
+      this.background =
+        'linear-gradient(120deg, #271d32, #3d3275, rgba(60,82,195,1))'
       setTimeout(this.syncTheme, 60000)
     },
   },
@@ -49,30 +60,35 @@ export default {
     TodoDisplayComponent,
     ForecastComponent,
     TodoCreateComponent,
-    DoneDisplayComponent,
   },
 }
 </script>
 
 <template>
   <main id="home-main">
-    <div class="wrapper">
+    <div class="main-wrapper">
       <div id="theme-picture"></div>
-      <QuoteComponent id="quote"></QuoteComponent>
-      <TodoCreateComponent id="todo"></TodoCreateComponent>
-      <ForecastComponent id="forecast"></ForecastComponent>
-      <TodoDisplayComponent></TodoDisplayComponent>
-      <DoneDisplayComponent></DoneDisplayComponent>
+      <QuoteComponent id="quote" class="container"></QuoteComponent>
+      <TodoCreateComponent id="todo" class="container"></TodoCreateComponent>
+      <ForecastComponent id="forecast" class="container"></ForecastComponent>
+      <TodoDisplayComponent id="list" class="container"></TodoDisplayComponent>
     </div>
   </main>
 </template>
 <style scoped>
+.container:before {
+  box-shadow: v-bind(brightness);
+}
 #home-main {
   display: flex;
   gap: 2rem;
   padding: 1rem;
   padding: 1rem;
+  background: v-bind(background);
   background-attachment: fixed;
+  box-sizing: border-box;
+  height: 95%;
+  box-sizing: border-box;
 }
 #theme-picture {
   background-image: v-bind(clock);
@@ -83,7 +99,7 @@ export default {
   grid-column: 1;
   grid-row: 1;
 }
-.wrapper {
+.main-wrapper {
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   grid-template-rows: 1fr 1fr;
@@ -102,5 +118,10 @@ export default {
 #forecast {
   grid-column: 2;
   grid-row: 2;
+}
+#list {
+  grid-row-start: 1;
+  grid-row-end: 2 span;
+  grid-column: 3;
 }
 </style>
