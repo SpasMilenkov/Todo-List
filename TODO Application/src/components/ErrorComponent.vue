c2a761958cf669fde789c238de49fce4410af721
Spas Milenkov <87969078+Clash2453@users.noreply.github.com>
2023-02-16 23:18:25 +0200

    Add error and loading components

---
+++ TODO Application/src/components/ErrorComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    <h1 class="main-title"><span id="error">:^(</span> Something went wrong</h1>
+  </div>
+</template>
+
+<style scoped>
+#error {
+  font-size: 10rem;
+}
+</style>
