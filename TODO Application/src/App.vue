c59ccae2a68c570038ba454e1ffbc548b925d5d7
Spas Milenkov <spasmilenkov123@gmail.com>
2023-02-19 11:53:50 +0200

    Separate the navbar in a component

--- TODO Application/src/App.vue
+++ TODO Application/src/App.vue
@@ -1,40 +1,9 @@
 <script setup>
 import HomeView from './views/HomeView.vue'
-import ClockComponent from './components/ClockComponent.vue'
+import Navigation from './components/GenericComponents/NavigationComponent.vue'
 </script>
 
 <template>
-  <nav id="navigation" class="container">
-    <div class="wrapper">
-      <ClockComponent></ClockComponent>
-    </div>
-    <ul id="navigation-container">
-      <li><a href="">Dummy 1</a></li>
-      <li><a href="">Dummy 2</a></li>
-    </ul>
-  </nav>
-  <HomeView></HomeView> 
+  <Navigation></Navigation>
+  <HomeView></HomeView>
 </template>
-
-<style scoped>
-#navigation {
-  height: 5%;
-  border-radius: 0px;
-  box-sizing: border-box;
-}
-#home {
-  height: 95%;
-}
-#navigation-container {
-  width: 90%;
-  height: 5%;
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  gap: 1rem;
-}
-.wrapper {
-  width: 20vw;
-  min-width: 15rem;
-}
-</style>
